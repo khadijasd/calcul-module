68456d97db78a6db67022728fff195f4812c1f16
khadijasd <khadijasaiidi2003@gmail.com>
2025-07-23 08:48:31 +0100

    Développement version 2

--- app/api/v1/calcul.py
+++ app/api/v1/calcul.py
@@ -6,6 +6,8 @@ from app.models.result import Result
 from app.models.single_calculation_request import SingleCalculationRequest
 from app.services.score import calculate_score_for_employee
 from app.services.score import calculate_score, get_top_employees
+from app.services.alternative_score import calculate_alternative_score_for_employee
+
 
 router = APIRouter()
 
@@ -24,3 +26,10 @@ def calculate_top(job_description: JobDescription, employees: List[Employee], th
     results = calculate_score(job_description, employees)
     top = get_top_employees(results, threshold=threshold)
     return top
+
+
+
+@router.post("/calculate/one/alt", response_model=Result)
+def calculate_one_alternative(req: SingleCalculationRequest):
+    return calculate_alternative_score_for_employee(req.job_description, req.employee)
+
