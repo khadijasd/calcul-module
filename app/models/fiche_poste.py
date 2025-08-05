c2d50a5d2b962bec42519940db48f62c271afe77
khadijasd <khadijasaiidi2003@gmail.com>
2025-08-05 22:53:43 +0100

    Développement version 3

--- app/models/fiche_poste.py
+++ app/models/fiche_poste.py
@@ -7,8 +7,12 @@ class RequiredSkillLevel(BaseModel):
     level_id: int
     level_value: int
     weight: float
-    type: Literal["must_have", "nice_to_have"]  # "must_have" ou "nice_to_have"
+    type: Literal["must_have", "nice_to_have"]  
+    
+
 
 class JobDescription(BaseModel):
     job_description_id: int
     required_skills_level: List[RequiredSkillLevel]
+    must_have_weight: float  # e.g., 0.7
+    nice_to_have_weight: float  # e.g., 0.3
